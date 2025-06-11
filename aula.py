e27188e5300fae5bcc4650c284a7bb8fe796e0c7
unknown <jorgems.alba@gmail.com>
2025-06-10 21:47:52 -0300

    envie para o github

---
+++ aula.py
@@ -0,0 +1,4 @@
+n = int(input("Infome um número:"))
+for i in range(1, 11):
+    multiplicacao = n * i 
+    print(f'{n} x {i} = {multiplicacao}')
---
+++ beecrowd.py/1002.py
@@ -0,0 +1,3 @@
+raio = int(input("Escreva o raio: "))
+pi = 3.14159
+print(f"A={pi * (raio ** 2)}")
