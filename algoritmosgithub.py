484c1f664e8c407d7bfbe41f4f689139b919a55d
unknown <20251PVAI10030005@estudantes.ifpr.edu.br>
2025-06-24 21:15:02 -0300

    dskjaldhaksjhdkjalshdlkjashdjkalh

--- algoritmosgithub.py
+++ algoritmosgithub.py
@@ -3,27 +3,27 @@ import os
 
 tamanho = 0
 
-while(tamanho < 5):
-    email = input("Insira seu usuário de E-Mail: ")
-    tamanho = len(email)
-    if(tamanho <= 5):
-        print("Informe E-mail com mais de 5 caracteres")
-print("Escolha a opção correspondente ao seu provedor de E-mails a seguir.")
-opcoes = int(input("1. ifpr.edu.br\n2.gmail.com\n3.hotmail.com\n4.Outra opcao: "))
-if(opcoes == 1):
-    email = email + "@ifpr.edu.br"
-elif(opcoes == 2):
-    email = email + "@gmail.com"
-elif(opcoes == 3):
-    email = email + "@hotmail.com"
-else: 
+# while(tamanho < 5):
+#     email = input("Insira seu usuário de E-Mail: ")
+#     tamanho = len(email)
+#     if(tamanho <= 5):
+#         print("Informe E-mail com mais de 5 caracteres")
+# print("Escolha a opção correspondente ao seu provedor de E-mails a seguir.")
+# opcoes = int(input("1. ifpr.edu.br\n2.gmail.com\n3.hotmail.com\n4.Outra opcao: "))
+# if(opcoes == 1):
+#     email = email + "@estudantesifpr.edu.br"
+# elif(opcoes == 2):
+#     email = email + "@gmail.com"
+# elif(opcoes == 3):
+#     email = email + "@hotmail.com"
+# else: 
 
-    servico = input("Digite o sufixo de seu usuário de email: ")
-    while(not(".com" in servico)):
-        servico = input("Digite o sufixo de seu usuário de email")
+#     servico = input("Digite o sufixo de seu usuário de email: ")
+#     while(not(".com" in servico)):
+#         servico = input("Digite o sufixo de seu usuário de email")
 
-    email = email + "@" + servico
-
+#     email = email + "@" + servico
+email = "20251PVAI10030005@estudantes.ifpr.edu.br"
 print(f"Seu E-Mail é {email}")
 
 correto = input("Seu E-Mail está correto? (S) Sim ou (N) Não? ")
---
+++ aula2.py
@@ -0,0 +1,29 @@
+#python não tem vetor, mas tem lista(que é mais flexiveç,é mais versatil)
+#vetores: são variaveis, possuem muitos valores e todos devem possuir o mesmo tipo de dado (não pode ser int ou float ou bool, apenas um desses
+#indice: número dentros dos colchetes ([]), representa a posição que estamos acessando dentro do vetor
+#obs: em python, a primeira posição é representada pelo índice 0 e a última posição é representada pelo indice n - 1 (n = tamanho do vetor)
+
+
+
+
+aluno = ["Jorge", "Caio", "Manoel"]
+print(aluno)
+y = int(input("Quantos alunos tem nessa turma? "))
+lista[0] = [""] * y
+lista[0] = "Jorge" 
+print("Na posição", 0 "está o aluno", lista[0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
